9edf6f3a148c069342142194683c314ecdde15e3
bim-g <ibmussafb@gmail.com>
2023-07-26 23:34:18 +0200

    [ENH] Update project validation by option resolver.

--- example/index.php
+++ example/index.php
@@ -5,15 +5,16 @@ include __DIR__ . '/../vendor/autoload.php';
 use Wepesi\App\DB;
 
 $config = [
-    "host" => "127.0.0.1",
-    "db_name" => "wepesi_db",
-    "username" => "root",
-    "password" => "Bobek@09"
+    'host' => '127.0.0.1',
+    'db_name' => 'wepesi_db',
+    'username' => 'root',
+    'password' => 'Bobek@09',
+    'port' => 3306
 ];
 $db = DB::getInstance($config);
 // include("insert.php");
-//include("select.php");
+include("select.php");
 // include("delete.php");
 // include("query.php");
 // include("update.php");
- include("transaction.php");
+// include("transaction.php");
